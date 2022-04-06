3703b7eee403355f7f941528369353de5e2231f7
Reniel Christianson <waldochristianson@gmail.com>
2022-04-06 17:36:01 -0500

    newest added codes to hw 5

---
+++ script.js
@@ -0,0 +1,8 @@
+let timeDisplayEl = $('#time-display');
+
+function displayTime() {
+    let rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);  
+    console.log(displayTime);
+}
+setInterval(displayTime, 1000);
